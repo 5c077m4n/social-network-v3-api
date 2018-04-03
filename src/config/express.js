e84b328de1a4c4fb9d8945768bf0ef21f4d9a520
Roee <roy.shap1@gmail.com>
2018-04-03 10:21:06 +0300

    Changed mongoDB server.

--- src/config/express.js
+++ src/config/express.js
@@ -11,8 +11,8 @@ const compress = require('compression');
 const middleware = require('./middleware');
 const cors = require('./middleware/cors');
 
-// const dbURI = 'mongodb://social:qwerty_123@ds111319.mlab.com:11319/social';
-const dbURI = 'mongodb://127.0.0.1:27017/social';
+const dbURI = 'mongodb://social:qwerty_123@ds111319.mlab.com:11319/social';
+// const dbURI = 'mongodb://127.0.0.1:27017/social';
 
 mongoose.connect(dbURI)
 	.then(() => {console.log('You have been successfully connected to the database.')})
